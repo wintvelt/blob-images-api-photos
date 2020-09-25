7be53808ae7d2909a29c36c1212052e6a60b18d4
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-09-25 12:25:11 +0200

    fix s3 permissions

--- handlersPhoto/createPhoto.js
+++ handlersPhoto/createPhoto.js
@@ -12,6 +12,7 @@ import { getMemberRole } from "../libs/dynamodb-lib-single";
 export const main = handler(async (event, context) => {
     const eventList = event.Records || [];
     console.log(eventList.map(item => item.s3?.object));
+    console.log(process.env.photoBucket, process.env.devBucket);
     const keyList = eventList.map(item => decodeURIComponent(item.s3.object.key));
     const keyListLength = keyList.length;
 
@@ -25,7 +26,6 @@ export const main = handler(async (event, context) => {
             [...userKeyList, key]
             : [key];
     }
-    console.log(keyListByUser);
     // update per user
     const userList = Object.keys(keyListByUser);
     const userListLength = userList.length;
