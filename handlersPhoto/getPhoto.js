e7b20eac2c0cd3ab356756bb6cd131bc7591e6e1
W.A. In 't Velt <wintvelt@WAs-MacBook-Pro.fritz.box>
2020-10-11 13:08:52 +0200

    getPhoto from other users + blob-common updpate

--- handlersPhoto/getPhoto.js
+++ handlersPhoto/getPhoto.js
@@ -1,12 +1,12 @@
 import { handler, getUserFromEvent } from "blob-common/core/handler";
 import { cleanRecord } from "blob-common/core/dbClean";
-import { getPhotoByUser } from "../libs/dynamodb-lib-single";
+import { getPhotoById } from "../libs/dynamodb-lib-single";
 
 export const main = handler(async (event, context) => {
     const userId = getUserFromEvent(event);
     const photoId = event.pathParameters.id;
-    // get photo
-    const photo = await getPhotoByUser(photoId, userId);
+    // get photo - also return photo if owned by someone else, but user has access
+    const photo = await getPhotoById(photoId, userId);
     if (!photo) throw new Error('photo not found');
 
     return cleanRecord(photo);
