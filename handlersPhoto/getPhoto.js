4b200c134a8633cc8322fb3555f0931686836acd
Wouter <wintvelt@me.com>
2021-09-16 22:29:50 +0200

    add signed Url to photo get

--- handlersPhoto/getPhoto.js
+++ handlersPhoto/getPhoto.js
@@ -1,13 +1,20 @@
 import { handler, getUserFromEvent } from "blob-common/core/handler";
 import { cleanRecord } from "blob-common/core/dbClean";
+import { getSignedUrlGet } from "blob-common/core/s3d";
 import { getPhotoById } from "../libs/dynamodb-lib-single";
 
 export const main = handler(async (event, context) => {
     const userId = getUserFromEvent(event);
     const photoId = event.pathParameters.id;
     // get photo - also return photo if owned by someone else, but user has access
-    const photo = await getPhotoById(photoId, userId);
+    let photo = await getPhotoById(photoId, userId);
     if (!photo) throw new Error('photo not found');
+
+    // add signed s3 url
+    const signedUrl = getSignedUrlGet({
+        Key: photo.url
+    });
+    photo.signedUrl = signedUrl;
 
     return cleanRecord(photo);
 });
