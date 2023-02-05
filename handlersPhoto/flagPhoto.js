e9f9f9bbc721344f3aed067fd3577d0750ce9818
Wouter <wintvelt@me.com>
2023-02-05 20:54:05 +0100

    add flagPhoto API

---
+++ handlersPhoto/flagPhoto.js
@@ -0,0 +1,21 @@
+// flag photo as inappropriate
+import { handler, getUserFromEvent } from "blob-common/core/handler";
+import { cleanRecord } from "blob-common/core/dbClean";
+import { getPhotoById } from "../libs/dynamodb-lib-single";
+import { dbUpdateMulti } from "blob-common/core/db";
+import { now } from "blob-common/core/date";
+
+export const main = handler(async (event, context) => {
+    const userId = getUserFromEvent(event);
+    const photoId = event.pathParameters.id;
+    // get photo - also return photo if owned by someone else, but user has access
+    let photo = await getPhotoById(photoId, userId);
+    if (!photo) throw new Error('photo not found');
+
+    await dbUpdateMulti(photo.PK, photo.SK, {
+        flaggedDate: now(),
+        flaggedBy: userId
+    });
+
+    return cleanRecord(photo);
+});
