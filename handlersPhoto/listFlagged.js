0337ebbfe461db0b43864862c8b62182e6050ae0
Wouter <wintvelt@me.com>
2023-02-06 12:46:11 +0100

    add list flagged photo api

---
+++ handlersPhoto/listFlagged.js
@@ -0,0 +1,10 @@
+// Returns personal photos flagged by others
+import { handler, getUserFromEvent } from "blob-common/core/handler";
+import { listPhotosByDate } from "../libs/dynamodb-query-lib";
+
+export const main = handler(async (event, context) => {
+    const userId = getUserFromEvent(event);
+    const photos = await listPhotosByDate(userId)
+        .filter(photo => !!photo.flaggedDate);
+    return photos;
+});
